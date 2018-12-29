b10b84f7f787e335a87f3725f479db2ab94398af
Ilya Fedorchenko <ilya.fedorchenko@gmail.com>
2018-12-29 14:48:40 +0300

    Initial Commit

---
+++ Eateries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eateries
+//
+//  Created by Илья Федорченко on 29/12/2018.
+//  Copyright © 2018 Илья Федорченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
