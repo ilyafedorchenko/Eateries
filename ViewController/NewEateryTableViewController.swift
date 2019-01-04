2d9332b09a9fb8b1f748f11d5262f667f5882310
Ilya Fedorchenko <ilya.fedorchenko@gmail.com>
2019-01-04 03:42:59 +0300

    L38

--- ViewController/NewEateryTableViewController.swift
+++ ViewController/NewEateryTableViewController.swift
@@ -8,84 +8,75 @@
 
 import UIKit
 
-class NewEateryTableViewController: UITableViewController {
+class NewEateryTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var adressTextField: UITextField!
+  @IBOutlet weak var typeTextField: UITextField!
+  @IBOutlet weak var yesButton: UIButton!
+  @IBOutlet weak var noButton: UIButton!
+  
+  @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+    if nameTextField.text == "" || adressTextField.text == "" || typeTextField.text == "" {
+      print("Some fields are empty")
+    } else {
+      performSegue(withIdentifier: "unwindSegueFromNewEatery", sender: self)
+    }
+  }
+  
+  @IBAction func toggleIsVisitedPressed(_ sender: UIButton) {
+    if sender == yesButton{
+      sender.backgroundColor = #colorLiteral(red: 0.3084011078, green: 0.5618229508, blue: 0, alpha: 1)
+      noButton.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    } else {
+      sender.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+      yesButton.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    }
+  }
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     tableView.tableFooterView = UIView(frame: CGRect.zero)
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
   }
   
-  // MARK: - Table view data source
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 1
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    imageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    dismiss(animated: true, completion: nil)
   }
   
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
-    return 5
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.row == 0 {
+      let alertController = UIAlertController(title: "Image source", message: nil, preferredStyle: .actionSheet)
+      let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {(action) in
+        self.chooseImagePickerAction(source: .camera)
+      })
+      let photoLibAction = UIAlertAction(title: "Photo Library", style: .default, handler: {(action) in
+        self.chooseImagePickerAction(source: .photoLibrary)
+      })
+      let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+      
+      alertController.addAction(cameraAction)
+      alertController.addAction(photoLibAction)
+      alertController.addAction(cancelAction)
+      present(alertController, animated: true, completion: nil)
+    }
+    tableView.deselectRow(at: indexPath, animated: true)
   }
   
-  /*
-   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-   
-   // Configure the cell...
-   
-   return cell
-   }
-   */
-  
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-  
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  func chooseImagePickerAction(source: UIImagePickerController.SourceType) {
+    if UIImagePickerController.isSourceTypeAvailable(source) {
+      let imagePicker = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.allowsEditing = true
+      imagePicker.sourceType = source
+      present(imagePicker, animated: true, completion: nil)
+      
+    }
+  }
   
 }
